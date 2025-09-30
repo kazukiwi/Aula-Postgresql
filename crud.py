55de6a353709741067b17e3d96e134e44aa4666a
Kazuki Vitor Shiroma <kazukishiroma06@gmail.com>
2025-09-30 11:15:39 -0300

    função listar alunos no banco ✔

--- crud.py
+++ crud.py
@@ -15,3 +15,18 @@ def criar_aluno(nome, idade):
             cursor.close()
             conexao.close()
 
+def listar_aluno():
+    conexao, cursor = conectar()
+    if conexao:  
+        try:
+            cursor.execute("""
+        SELECT * FROM alunos ORDER BY id
+        """)
+            return cursor.fetchall()
+        except Exception as erro:
+            print(f"Erro ao listar os alunos: {erro}")
+            return []
+        finally:
+            cursor.close()
+            conexao.close()
+
